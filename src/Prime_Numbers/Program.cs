3d0c3e357254aa4ba3fa2fc524c130e3bad97a24
Hamidalion <fearofwar@mail.ru>
2021-01-18 20:35:36 +0300

    feat: Add Prime Number

---
+++ src/Prime_Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                bool condition = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        condition = false;
+                    }
+                }
+                if (condition)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
