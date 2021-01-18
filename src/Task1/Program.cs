490dfa907e35b56bb3467c609a90f403f4ffaf7f
Hamidalion <fearofwar@mail.ru>
2021-01-18 19:18:37 +0300

    init: Recursion

---
+++ src/Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            #region withoutRecursion
+
+            //int factorial = 1;
+
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    factorial *= i;
+
+            //    if (i == number)
+            //    {
+            //        Console.Write($"{i}=");
+            //    }
+            //    else
+            //    {
+            //        Console.Write($"{i}*");
+            //    }
+            //}
+            //Console.Write(factorial);
+            //Console.ReadKey();
+            //return 0;
+            #endregion
+
+            #region withRecursion
+            Console.WriteLine("Factorial number {0} = {1}", number, factorial(number));
+            Console.ReadLine();
+            #endregion
+        }
+
+        static int factorial(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * factorial(number - 1);
+            }
+        }
+    }
+}
